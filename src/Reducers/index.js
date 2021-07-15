c24295c94f6cf186f01ec5c1281f63e45c2f14a9
Diego <diegodsha@hotmail.com>
2021-07-15 16:32:36 -0500

    Combine book reducer

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,6 +1,6 @@
-import { createStore } from 'redux';
+import { combineReducers } from 'redux';
 import bookReducer from './books';
 
-const store = createStore(bookReducer);
+const rootReducer = combineReducers({ bookReducer });
 
-export default store;
+export default rootReducer;
