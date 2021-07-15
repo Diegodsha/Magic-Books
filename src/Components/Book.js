3d76d31e228d4b72786402eacd6bdc6abd5f6a1f
Diego <diegodsha@hotmail.com>
2021-07-15 16:31:51 -0500

    Update book component structure

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Book = () => (
-  <div>book</div>
+const Book = ({ book }) => (
+  <tr>
+    <th scope="row">{book.id}</th>
+    <td>{book.title}</td>
+    <td>{book.category}</td>
+  </tr>
 );
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    category: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default Book;
