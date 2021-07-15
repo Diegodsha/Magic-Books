2eff57a5a877456f0fdfc557081aca6c97bcc4e2
Diego <diegodsha@hotmail.com>
2021-07-15 16:33:25 -0500

    Implement initial state

--- src/index.js
+++ src/index.js
@@ -2,8 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './Components/App';
-import store from './Reducers/index';
+import rootReducer from './Reducers/index';
+
+const idGenerator = () => Math.floor(Math.random() * 500);
+
+const bookReducer = [
+  {
+    id: idGenerator(),
+    title: 'Clean Code',
+    category: 'Learning',
+  },
+  {
+    id: idGenerator(),
+    title: 'The Shinning',
+    category: 'Horror',
+  },
+  {
+    id: idGenerator(),
+    title: 'The Martian',
+    category: 'Sci-Fi',
+  },
+];
+
+const intialState = { bookReducer };
+
+const store = createStore(rootReducer, intialState);
 
 ReactDOM.render(
   <Provider store={store}>
