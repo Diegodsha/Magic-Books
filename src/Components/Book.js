0127b08b4481c7e05c3d81a125ea7c85a61f4374
Diego <diegodsha@hotmail.com>
2021-07-16 15:26:54 -0500

    Add react-icons and remove action onClick

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FaTrashAlt } from 'react-icons/fa';
 
-const Book = ({ book }) => (
+const Book = ({ book, removeBook }) => (
   <tr>
     <th scope="row">{book.id}</th>
     <td>{book.title}</td>
     <td>{book.category}</td>
+    <td>
+      <button onClick={() => removeBook(book)} type="button" className="btn btn-sm btn-danger">
+        <FaTrashAlt className="text-white trash" />
+      </button>
+    </td>
   </tr>
 );
 
@@ -15,6 +21,7 @@ Book.propTypes = {
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
   }).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 
 export default Book;
