1fccce895c3ce4c0cd9cb9b24351556f756f7af0
Diego <diegodsha@hotmail.com>
2021-07-16 16:45:05 -0500

    Add redux devtools extension

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension/logOnlyInProduction';
 import App from './Components/App';
 import rootReducer from './Reducers/index';
 import idGenerator from './Helpers/IdGenerator';
@@ -30,8 +31,7 @@ const intialState = { bookReducer };
 const store = createStore(
   rootReducer,
   intialState,
-  // eslint-disable-next-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  devToolsEnhancer(),
 );
 
 ReactDOM.render(
