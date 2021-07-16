fd4a650ee878273405df73a1a22d39d15f04bce4
Diego <diegodsha@hotmail.com>
2021-07-16 15:26:17 -0500

    Connect removeAction to button

--- src/Container/BookList.js
+++ src/Container/BookList.js
@@ -1,8 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Book from '../Components/Book';
+import { removeAction } from '../Actions/index';
 
 const BookList = () => {
   const books = useSelector((state) => state.bookReducer);
+  const dispatch = useDispatch();
+  const handleRemoveBook = (book) => {
+    dispatch(removeAction(book));
+  };
 
   return (
     <div className="col-8 mt-4">
@@ -12,11 +17,12 @@ const BookList = () => {
             <th scope="col">ID</th>
             <th scope="col">Title</th>
             <th scope="col">Category</th>
+            <th scope="col"> </th>
           </tr>
         </thead>
         <tbody>
           {books.map((book) => (
-            <Book key={book.id} book={book} />
+            <Book key={book.id} book={book} removeBook={handleRemoveBook} />
           ))}
         </tbody>
       </table>
