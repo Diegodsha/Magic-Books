2a6d16186370e81f5d70e0e8894a8e9d59a9810d
Diego <diegodsha@hotmail.com>
2021-07-20 17:52:39 -0500

    Update book style

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FaTrashAlt } from 'react-icons/fa';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { BookCol } from '../StyledComponents/styles';
 
 const Book = ({ book, removeBook }) => (
-  <tr>
-    <th scope="row">{book.id}</th>
-    <td>{book.title}</td>
-    <td>{book.category}</td>
-    <td>
-      <button onClick={() => removeBook(book)} type="button" className="btn btn-sm btn-danger">
-        <FaTrashAlt className="text-white trash" />
-      </button>
-    </td>
-  </tr>
+  <BookCol className="col-12 my-2">
+    <div className="row justify-content-between align-items-center">
+      <div className="col-12 col-md-4">
+        <div className="d-flex flex-column">
+          <span className="book-category">{book.category}</span>
+          <span className="book-title">{book.title}</span>
+          <span className="book-author edit-book mb-2">
+            {book.author ?? 'Author'}
+          </span>
+        </div>
+        <div>
+          <span className="comments edit-book pe-3">Comments</span>
+          <span>
+            <button
+              className="remove edit-book px-3"
+              onClick={() => removeBook(book)}
+              type="button"
+            >
+              Remove
+            </button>
+          </span>
+          <span className="edit edit-book ps-3">Edit</span>
+        </div>
+      </div>
+      <div className="col-12 col-md-8 content">
+        <div className="percentage d-flex">
+          <CircularProgressbar value={5} text={`${5}%`} />
+          <div className="d-flex flex-column ">
+            <span>0%</span>
+            <span>Completed</span>
+          </div>
+        </div>
+        <span className="divider" />
+        <div className="d-flex flex-column">
+          <span className="chapter">CURRENT CHAPTER</span>
+          <span>Chapter</span>
+          <button className="update-btn" type="button">
+            UPDATE PROGRESS
+          </button>
+        </div>
+      </div>
+    </div>
+  </BookCol>
 );
 
 Book.propTypes = {
@@ -20,6 +55,7 @@ Book.propTypes = {
     id: PropTypes.number.isRequired,
     title: PropTypes.string.isRequired,
     category: PropTypes.string.isRequired,
+    author: PropTypes.string.isRequired,
   }).isRequired,
   removeBook: PropTypes.func.isRequired,
 };
