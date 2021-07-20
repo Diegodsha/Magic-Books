c77d1632d863bc01cdaedfc4e787400acdd79e0f
Diego <diegodsha@hotmail.com>
2021-07-20 17:51:55 -0500

    Add nav to app

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,11 @@
 import BookForm from '../Container/ BooksForm';
 import BookList from '../Container/BookList';
+import Nav from './Nav';
 
 function App() {
   return (
     <div className="App justify-content-center row ">
+      <Nav />
       <BookList />
       <BookForm />
     </div>
