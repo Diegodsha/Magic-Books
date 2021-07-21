e05bd96f87508fadad059e0607acc5388cb2ca16
Diego <diegodsha@hotmail.com>
2021-07-21 13:40:25 -0500

    Style book form

--- src/Container/ BooksForm.js
+++ src/Container/ BooksForm.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createAction } from '../Actions/index';
 import { idGenerator, randNum } from '../Helpers/helpers';
+import { StyledForm } from '../StyledComponents/styles';
 
 const categories = [
   'Action',
@@ -33,11 +34,7 @@ const BookForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (Book.title === '') {
-      const alert = document.querySelector('.alert');
-      if (alert) {
-        alert.classList.add('show');
-      }
+    if (Book.category === '') {
       return;
     }
 
@@ -46,40 +43,53 @@ const BookForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="col-12">
-      <div className="mb-3">
-        <label htmlFor="bookTitle" className="form-label">
-          ADD NEW BOOK
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="bookTitle"
-          value={Book.title}
-          placeholder="Book title"
-          onChange={handleChange}
-        />
-      </div>
-      <div className="mb-3">
-        <select
-          onChange={handleChange}
-          id="bookCategory"
-          className="form-select"
-          aria-label="Default select example"
-        >
-          <option defaultValue>Select category</option>
-          {categories.map((category) => (
-            <option value={category} key={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
+    <>
+      <div
+        className="divider-2"
+        style={{
+          width: '91%',
+          height: '0.125rem',
+          margin: '2.5rem 0.063rem 1.813rem 0,',
+          border: 'solid 1px #e8e8e8',
+        }}
+      />
+      <StyledForm
+        onSubmit={handleSubmit}
+        className="row p-2 px-md-5 py-2 py-md-4"
+      >
+        <div className=" add-book">ADD NEW BOOK</div>
+        <div className="col-5 mb-3">
+          <input
+            type="text"
+            className="form-control"
+            id="bookTitle"
+            value={Book.title}
+            placeholder="Book title"
+            required
+            onChange={handleChange}
+          />
+        </div>
+        <div className=" col-4 mb-3">
+          <select
+            onChange={handleChange}
+            id="bookCategory"
+            className="form-select"
+            aria-label="Default select example"
+          >
+            <option defaultValue>Category</option>
+            {categories.map((category) => (
+              <option value={category} key={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
 
-      <button type="submit" className="btn btn-primary">
-        Add book
-      </button>
-    </form>
+        <button type="submit" className="btn btn-primary col-3">
+          Add book
+        </button>
+      </StyledForm>
+    </>
   );
 };
 
