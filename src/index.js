3e93232dd6a45c3c1c2867e6084b232129105fa1
Diego <diegodsha@hotmail.com>
2021-07-22 17:49:53 -0500

    Add redux-thunk and axios for asyn operations

--- src/index.js
+++ src/index.js
@@ -2,43 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './Components/App';
 import rootReducer from './Reducers/index';
-import { idGenerator, randNum } from './Helpers/helpers';
+// import { idGenerator, randNum } from './Helpers/helpers';
+// import { API_URL, getBooks } from './api/api';
 
-const bookReducer = [
-  {
-    id: idGenerator(),
-    title: 'Clean Code',
-    category: 'Learning',
-    author: 'Robert Cecil Martin',
-    progress: randNum(),
-  },
-  {
-    id: idGenerator(),
-    title: 'The Shinning',
-    category: 'Horror',
-    author: 'Stephen King',
-    progress: randNum(),
-  },
-  {
-    id: idGenerator(),
-    title: 'The Martian',
-    category: 'Sci-Fi',
-    author: 'Andy Weir',
-    progress: randNum(),
-  },
-];
+// const bookReducer = [
+//   {
+//     id: idGenerator(),
+//     title: 'Clean Code',
+//     category: 'Learning',
+//     author: 'Robert Cecil Martin',
+//     chapter: 13,
+//     progress: randNum(),
+//   },
+//   {
+//     id: idGenerator(),
+//     title: 'The Shinning',
+//     category: 'Horror',
+//     author: 'Stephen King',
+//     chapter: 9,
+//     progress: randNum(),
+//   },
+//   {
+//     id: idGenerator(),
+//     title: 'The Martian',
+//     category: 'Sci-Fi',
+//     author: 'Andy Weir',
+//     chapter: 5,
+//     progress: randNum(),
+//   },
+// ];
 
-const intialState = { bookReducer };
+// const intialState = { bookReducer };
 
-const store = createStore(
-  rootReducer,
-  intialState,
-  devToolsEnhancer(),
-);
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
